64c30d6b4cb62101cbbbb29dbe55c52fa10a84d3
EdwinYee <edwinyee@ucsb.edu>
2022-10-10 17:13:49 -0700

    Getting started with mouseclick and drawing board

--- Connect.py
+++ Connect.py
@@ -14,6 +14,12 @@ BLACK = (0, 0, 0)
 NUM_ROWS = 8
 NUM_COLS = 8
 
+# Initializing surface
+SURFACE_WIDTH = 400
+SURFACE_HEIGHT = 400
+surface = pygame.display.set_mode((SURFACE_WIDTH, SURFACE_HEIGHT))
+
+game_over = False
 
 def initialize_board():
     game_board = np.zeros((NUM_ROWS, NUM_COLS))
@@ -27,6 +33,46 @@ def is_valid_position(game_board, row, col):
         return False  # Occupied space
     else:
         return True  # Not occupied
+
+
+def draw_board():
+    for row in range(NUM_ROWS):
+        for col in range(NUM_COLS):
+            pygame.draw.rect(surface, GREEN,
+            pygame.Rect(row*SURFACE_WIDTH/8, col*SURFACE_HEIGHT/8, SURFACE_WIDTH/8, SURFACE_HEIGHT/8))
+
+# Initializing Pygame
+pygame.init()
+
+# Initializing surface
+surface = pygame.display.set_mode((400, 300))
+
+# Initialing Color
+color = (255, 0, 0)
+
+# Initializing the board
+draw_board()
+pygame.display.flip()
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x_position, y_position = pygame.mouse.get_pos()
+            rect_width = 60
+            rect_height = 60
+
+            pygame.draw.rect(surface, RED, (x_position, y_position, rect_width, rect_height))
+            pygame.display.flip()
+
+
+
+
+
+
+
+
 
 
 
