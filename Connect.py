a64137523226bcbed6d51bc7af72e2c2318a8ab6
EdwinYee <edwinyee@ucsb.edu>
2022-10-14 17:50:28 -0700

    Experimenting with sprites

--- Connect.py
+++ Connect.py
@@ -4,6 +4,10 @@ import random
 import math
 import sys
 
+# References (for Python sprites):
+    # https://realpython.com/lessons/creating-sprites/
+    # https://www.geeksforgeeks.org/pygame-creating-sprites/
+
 # Defining colors
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -19,10 +23,19 @@ NUM_COLS = 8
 # Initializing surface
 SURFACE_WIDTH = 400
 SURFACE_HEIGHT = 400
-surface = pygame.display.set_mode((SURFACE_WIDTH, SURFACE_HEIGHT))
 
 CIRCLE_RAD = (SURFACE_WIDTH/8)/2
 game_over = False
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, sprite_color, width, height):
+        super(Sprite, self).__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(sprite_color)
+        self.rect = self.image.get_rect()
+
 
 def initialize_board():
     game_board = np.zeros((NUM_ROWS, NUM_COLS))
@@ -46,6 +59,8 @@ def draw_board():
         num = num + 1
         for col in range(NUM_COLS):
             num = num + 1
+
+            # Drawing the game board visuals
             if num % 2 == 1:
                 pygame.draw.rect(surface, DARK_BROWN,
                 pygame.Rect(row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8,
@@ -66,21 +81,29 @@ def draw_board():
                                                     col * SURFACE_HEIGHT / 8 + CIRCLE_RAD), CIRCLE_RAD)
 
 
-
-
-
-
-
 # Initializing Pygame
 pygame.init()
+surface = pygame.display.set_mode((SURFACE_WIDTH, SURFACE_HEIGHT))
 
+all_sprites_list = pygame.sprite.Group()
 
-# Initialing Color
-color = (255, 0, 0)
+# Creating sprites with color and dimensions
+sprite1 = Sprite(RED, 100, 100)
+sprite2 = Sprite(GREEN, 100, 200)
+sprite3 = Sprite(BLUE, 200, 100)
+
+sprite1.rect.x = 0
+sprite1.rect.y = 0
+sprite2.rect.x = 100
+sprite2.rect.y = 100
+sprite3.rect.x = 200
+sprite3.rect.y = 200
+
+all_sprites_list.add(sprite1, sprite2, sprite3)
 
 # Initializing the board
-draw_board()
-pygame.display.flip()
+# draw_board()
+# pygame.display.flip()
 
 while not game_over:
     for event in pygame.event.get():
@@ -93,6 +116,12 @@ while not game_over:
 
             pygame.draw.rect(surface, RED, (x_position, y_position, rect_width, rect_height))
             pygame.display.flip()
+
+    all_sprites_list.update()
+    all_sprites_list.draw(surface)
+
+    pygame.display.flip()
+
 
 
 
