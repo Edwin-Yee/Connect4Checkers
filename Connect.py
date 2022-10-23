1bf9d421ad1f51099ce72d939524520a558ad3a8
EdwinYee <edwinyee@ucsb.edu>
2022-10-22 22:10:20 -0700

    Game board no longer disappears after clicking on a piece

--- Connect.py
+++ Connect.py
@@ -35,6 +35,7 @@ indicator_group = pygame.sprite.Group
 pygame.init()
 surface = pygame.display.set_mode((SURFACE_WIDTH, SURFACE_HEIGHT))
 
+
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, sprite_color, width, height):
         super(Sprite, self).__init__()
@@ -75,6 +76,20 @@ class Button(pygame.sprite.Sprite):
     #         self.kill()  # Will remove itself from all pygame groups.
     #         surface.fill(WHITE)
     #         pygame.display.flip()
+
+
+class BoardSquare(pygame.sprite.Sprite):
+    def __init__(self, square_color, width, height):
+        super(BoardSquare, self).__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(square_color)
+
+        self.color = square_color
+
+        self.rect = self.image.get_rect()
+
+
 def initialize_board():
     game_board = np.zeros((NUM_ROWS, NUM_COLS))
     return game_board
@@ -94,8 +109,8 @@ def draw_board():
 
     red_pieces_coords = []
     black_pieces_coords = []
-
-    surface.fill(WHITE)
+    brown_board_square_coords = []
+    black_board_square_coords = []
 
     for row in range(NUM_ROWS):
         num = num + 1
@@ -104,26 +119,25 @@ def draw_board():
 
             # Drawing the game board visuals
             if num % 2 == 1:
-                pygame.draw.rect(surface, DARK_BROWN,
-                pygame.Rect(row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8,
-                SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8))
+                brown_board_square_coords.append((row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8))
+                # pygame.draw.rect(surface, DARK_BROWN,
+                # pygame.Rect(row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8,
+                # SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8))
             else:
-                pygame.draw.rect(surface, PALE_COLOR,
-                pygame.Rect(row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8,
-                SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8))
+                black_board_square_coords.append((row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8))
+                # pygame.draw.rect(surface, PALE_COLOR,
+                # pygame.Rect(row * SURFACE_WIDTH / 8, col * SURFACE_HEIGHT / 8,
+                # SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8))
 
             if col == 0 or col == 1:
-                # Generate coordinates for the sprite pieces
-                # Red pieces coordinates
+                # Generate red piece coordinates for the sprite
                 red_pieces_coords.append((row * SURFACE_WIDTH / 8 + CIRCLE_RAD, col * SURFACE_HEIGHT / 8 + CIRCLE_RAD))
 
                 # pygame.draw.circle(surface, RED, (row * SURFACE_WIDTH / 8 + CIRCLE_RAD,
                 #                                    col * SURFACE_HEIGHT / 8 + CIRCLE_RAD), CIRCLE_RAD)
 
-            # # Creating the Black Pieces as sprites
             if col == NUM_COLS-1 or col == NUM_COLS-2:
-                # Generate coordinates for the sprite pieces
-                # Black pieces coordinates
+                # Generate black piece coordinates for the sprite
                 black_pieces_coords.append((row * SURFACE_WIDTH / 8 + CIRCLE_RAD, col * SURFACE_HEIGHT / 8 + CIRCLE_RAD))
 
                 # pygame.draw.circle(surface, BLACK, (row * SURFACE_WIDTH / 8 + CIRCLE_RAD,
@@ -136,12 +150,23 @@ def draw_board():
         red_game_pieces["red_piece%d" % count].rect.x = coord[0] - CIRCLE_RAD/2
         red_game_pieces["red_piece%d" % count].rect.y = coord[1] - CIRCLE_RAD/2
 
-
     black_game_pieces = {}
     for count, coord in enumerate(black_pieces_coords):
-        red_game_pieces["black_piece%d" % count] = Sprite(BLACK, CIRCLE_RAD, CIRCLE_RAD)
-        red_game_pieces["black_piece%d" % count].rect.x = coord[0] - CIRCLE_RAD/2
-        red_game_pieces["black_piece%d" % count].rect.y = coord[1] - CIRCLE_RAD/2
+        black_game_pieces["black_piece%d" % count] = Sprite(BLACK, CIRCLE_RAD, CIRCLE_RAD)
+        black_game_pieces["black_piece%d" % count].rect.x = coord[0] - CIRCLE_RAD/2
+        black_game_pieces["black_piece%d" % count].rect.y = coord[1] - CIRCLE_RAD/2
+
+    brown_squares = {}
+    for count, coord in enumerate(brown_board_square_coords):
+        brown_squares["brown_square%d" % count] = BoardSquare(DARK_BROWN, SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8)
+        brown_squares["brown_square%d" % count].rect.x = coord[0]
+        brown_squares["brown_square%d" % count].rect.y = coord[1]
+
+    black_squares = {}
+    for count, coord in enumerate(black_board_square_coords):
+        black_squares["black_square%d" % count] = BoardSquare(PALE_COLOR, SURFACE_WIDTH / 8, SURFACE_HEIGHT / 8)
+        black_squares["black_square%d" % count].rect.x = coord[0]
+        black_squares["black_square%d" % count].rect.y = coord[1]
 
     for i in red_game_pieces:
         all_sprites_list.add(red_game_pieces[i])
@@ -149,6 +174,11 @@ def draw_board():
     for j in black_game_pieces:
         all_sprites_list.add(black_game_pieces[j])
 
+    for k in brown_squares:
+        squares_list.add(brown_squares[k])
+
+    for p in black_squares:
+        squares_list.add(black_squares[p])
 
 
 image = pygame.Surface((100, 40))
@@ -157,6 +187,8 @@ buttons = pygame.sprite.Group()
 
 # Legal moves are "jumping" over another piece in front of your piece. Pieces with no other pieces in
 # front will have no legal moves.
+
+
 def provide_legal_moves(x, y, color):
 
     print(x, y, color)
@@ -181,11 +213,14 @@ def provide_legal_moves(x, y, color):
             buttons.add(Button(BLUE, pos=(x + 4 * CIRCLE_RAD, y + 4 * CIRCLE_RAD), image=image))
 
 
-
 all_sprites_list = pygame.sprite.Group()
+squares_list = pygame.sprite.Group()
 
 # Initializing the board
 draw_board()
+squares_list.update()
+squares_list.draw(surface)
+
 pygame.display.flip()
 
 num_click = 0
@@ -219,12 +254,16 @@ while not game_over:
             num_click = num_click + 1
             pygame.display.flip()
 
-    buttons.update()  # Calls the update method on every sprite in the group.
+    # Update the board squares
+    squares_list.update()
+    squares_list.draw(surface)
 
-    # surface.fill((0, 0, 0))
-    buttons.draw(surface)  # Draws all sprites to the given Surface.
+    # Draws all sprites to the given Surface.
+    buttons.update()  # Calls the update method on every sprite in the group.
+    buttons.draw(surface)
     all_sprites_list.update()
     all_sprites_list.draw(surface)
+
     pygame.display.update()
 
 
